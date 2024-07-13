b0181fc64637f7063c09f92c356bcc6468c090ca
INNOCENT NZIMENYERA <36258940+Innocent50@users.noreply.github.com>
2024-07-13 21:04:36 +0200

    creation of two tables in database

--- example_schema.sql
+++ example_schema.sql
@@ -8,3 +8,21 @@ create table examples
             primary key,
     name varchar
 );
+-- Create places table
+CREATE TABLE places (
+    id SERIAL PRIMARY KEY,
+    city VARCHAR(255) NOT NULL,
+    county VARCHAR(255),
+    country VARCHAR(255) NOT NULL
+);
+
+-- Create people table
+CREATE TABLE people (
+    id SERIAL PRIMARY KEY,
+    given_name VARCHAR(255) NOT NULL,
+    family_name VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    place_of_birth INTEGER,
+    FOREIGN KEY (place_of_birth) REFERENCES places(id)
+);
+
